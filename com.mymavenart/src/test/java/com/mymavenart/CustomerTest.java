09225105c5d706dcc1d8686a8ea7ae69e5abf323
Prasad <Prasad@DNV>
2019-09-15 11:34:26 +0530

    My Commit 3

---
+++ com.mymavenart/src/test/java/com/mymavenart/CustomerTest.java
@@ -0,0 +1,24 @@
+package com.mymavenart;
+
+import org.testng.annotations.Test;
+
+public class CustomerTest {
+	
+	@Test
+	public void createCustomerTest()
+	{
+		System.out.println("Execute all Create Customer Scripts");
+	}
+	
+	@Test
+	public void modifyCustomerTest()
+	{
+		System.out.println("Execute all Modify Customer Scripts");
+	}
+	@Test
+	public void deleteCustomerTest()
+	{
+		System.out.println("Execute all Delete Customer Scripts");
+	}
+
+}
